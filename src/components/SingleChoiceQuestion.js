e9618ad372860e5bc88546143031b54f13f74a59
Csicsike76 <19perro76@gmail.com>
2025-08-07 20:16:54 +0300

    src\components\SingleChoiceQuestion.js

--- src/components/SingleChoiceQuestion.js
+++ src/components/SingleChoiceQuestion.js
@@ -1,73 +1,30 @@
 import React from 'react';
 import styles from './QuestionStyles.module.css';
 
-const SingleChoiceQuestion = ({ question, userAnswer, onAnswerChange, showResults }) => {
-    let options = [];
-
-    if (Array.isArray(question.options)) {
-        options = question.options; // már tömb
-    } else {
-        try {
-            const parsedOptions = JSON.parse(question.options);
-            if (Array.isArray(parsedOptions)) {
-                options = parsedOptions;
-            } else {
-                console.error("Nem tömb típusú opciók:", question.options);
-            }
-        } catch (e) {
-            console.error("Nem sikerült feldolgozni a JSON opciókat:", question.options, e);
-        }
-    }
-
-    const getIcon = (isCorrect, isSelected) => {
-        if (!showResults) return null;
-        if (isCorrect) return <span className={styles.icon}>✔</span>;
-        if (isSelected && !isCorrect) return <span className={styles.icon}>✘</span>;
-        return null;
-    };
+const SingleChoiceQuestion = ({ question, userAnswer, onAnswerChange }) => {
+    const options = Array.isArray(question.options) ? question.options : [];
 
     return (
         <div className={styles.questionBlock}>
             <p className={styles.description}>{question.description}</p>
             <div className={styles.optionsGrid}>
-                {options.map((option, index) => {
-                    let labelClass = styles.optionLabel;
-                    const isSelected = userAnswer === option;
-                    const isCorrect = question.answer === option;
-
-                    if (showResults) {
-                        if (isCorrect) {
-                            labelClass += ` ${styles.correct}`;
-                        } else if (isSelected && !isCorrect) {
-                            labelClass += ` ${styles.incorrect}`;
-                        }
-                    } else if (isSelected) {
-                        labelClass += ` ${styles.selected}`;
-                    }
-
-                    const icon = getIcon(isCorrect, isSelected);
-
-                    return (
-                        <label key={index} className={labelClass}>
-                            <input
-                                type="radio"
-                                name={`question-${question.id}`}
-                                value={option}
-                                checked={isSelected}
-                                onChange={() => onAnswerChange(question.id, option)}
-                                style={{ display: 'none' }}
-                                disabled={showResults}
-                            />
-                            {option} {icon}
-                        </label>
-                    );
-                })}
+                {options.map((option, index) => (
+                    <label 
+                        key={index} 
+                        className={`${styles.optionLabel} ${userAnswer === option ? styles.selected : ''}`}
+                    >
+                        <input
+                            type="radio"
+                            name={`question-${question.id}`}
+                            value={option}
+                            checked={userAnswer === option}
+                            onChange={() => onAnswerChange(question.id, option)}
+                            style={{ display: 'none' }}
+                        />
+                        {option}
+                    </label>
+                ))}
             </div>
-            {showResults && question.explanation && (
-                <div className={styles.explanation}>
-                    {question.explanation}
-                </div>
-            )}
         </div>
     );
 };
