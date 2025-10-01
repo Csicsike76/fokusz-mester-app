20133a95c353c7543da4b5bcc762098119793643
Csicsike76 <19perro76@gmail.com>
2025-10-01 19:41:33 +0300

    jkggf

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,8 @@ const whitelist = [
   process.env.FRONTEND_URL,
   'https://fokuszmester.com',
   'https://www.fokuszmester.com',
-  
+  'capacitor://localhost', // EZT ADD HOZZÁ
+  'http://localhost'       // EZT ADD HOZZÁ
 ];
 if (process.env.NODE_ENV !== 'production') {
   const os = require('os');
